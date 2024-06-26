43c09493a51535434f0d8f9ff6526c13d838566b
Lakshmipriya0404 <priya42003@gmail.com>
2024-06-26 20:36:49 +0530

    initial commit

---
+++ readers-junction/src/main/java/com/priya/readers/auth/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.priya.readers.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationResponse {
+    private String token;
+}
---
+++ readers-junction/src/test/java/com/priya/readers/ReadersJunctionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priya.readers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadersJunctionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
